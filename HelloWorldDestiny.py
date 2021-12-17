d89c203c94d99caf130f8311c024ba75d77ac246
destinydf <85776972+destinydf@users.noreply.github.com>
2021-12-17 15:13:51 -0500

    Add files via upload

---
+++ HelloWorldDestiny.py
@@ -0,0 +1,12 @@
+print('Hello there!')
+name = input('What is your name?: ')
+print('Welcome ' +name)
+weather = input('What is the weather like where you are?: ')
+print("I hope youre enjoying your " + weather + " weather.")
+temperature = int(input('Tell me, if you can check, what is the temperature today?: '))
+if temperature > 80:
+    print("Its going to be a pretty hot day. Dress comfortably.")
+elif temperature < 80:
+    print("Its going to be pretty cold today, dress warmly!")
+    print("Have a wonderful day!")
+#Destiny 12/15/2021
